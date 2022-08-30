7d25d5087df6bf4ba6d7b8ef8893db10d2fdde6e
MauriRios <87584747+MauriRios@users.noreply.github.com>
2022-08-29 21:12:39 -0300

    probando carrouseles

--- creationshopfe/src/app/components/sponsorship/sponsorship.component.ts
+++ creationshopfe/src/app/components/sponsorship/sponsorship.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sponsorship',
   templateUrl: './sponsorship.component.html',
@@ -10,6 +11,6 @@ export class SponsorshipComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+}
 }
