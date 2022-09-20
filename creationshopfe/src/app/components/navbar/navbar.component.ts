e4f566f812693971d5d405a09825e81c506dcf99
MauriRios <87584747+MauriRios@users.noreply.github.com>
2022-09-19 23:46:20 -0300

    offcanvas arreglado y agregue botones

--- creationshopfe/src/app/components/navbar/navbar.component.ts
+++ creationshopfe/src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isLogged = true
+  isAdmin = true
+
   constructor() { }
 
   ngOnInit(): void {
